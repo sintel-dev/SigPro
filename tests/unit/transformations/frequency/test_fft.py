a1094476108bfe637411ec7d7ddb5ac56709c236
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2020-11-13 15:36:29 +0100

    Add fft with complex return

--- tests/unit/transformations/frequency/test_fft.py
+++ tests/unit/transformations/frequency/test_fft.py
@@ -1,7 +1,21 @@
 """Tests for sigpro.transformations.frequency.fft module."""
 import numpy as np
 
-from sigpro.transformations.frequency.fft import fft_real
+from sigpro.transformations.frequency.fft import fft, fft_real
+
+
+def test_fft():
+    # setup
+    values = [1, 1, 1, 1, 1]
+
+    # run
+    amplitude_values, frequency_values = fft(values, 10)
+
+    # assert
+    expected_amplitude_values = [5. + 0.j, 0. + 0.j, 0. + 0.j, 0. + 0.j, 0. + 0.j]
+    expected_frequency_values = [0.0, 0.02, 0.04, -0.04, -0.02]
+    np.testing.assert_array_almost_equal(amplitude_values, expected_amplitude_values)
+    np.testing.assert_array_almost_equal(frequency_values, expected_frequency_values)
 
 
 def test_fft_real():
