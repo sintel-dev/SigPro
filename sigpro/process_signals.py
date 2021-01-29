ebabe7238f342d3934020206bb8db754cc704972
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-01-29 13:41:08 +0100

    Implement sigpro.process_signals and integration test

--- sigpro/process_signals.py
+++ sigpro/process_signals.py
@@ -1,33 +1,156 @@
 # -*- coding: utf-8 -*-
+"""Process Signals core functionality."""
+
+from collections import Counter
+
+import pandas as pd
+from mlblocks import MLPipeline, load_primitive
 
 
-def process_signals(data, primitives, target_column='values'):
+def _build_pipeline(transformations, aggregations):
+    """Build Pipeline function.
+
+    Given a list of transformations and aggregations build a pipeline
+    with the output of the aggregations, which take as name the specified
+    name of the transformations and the aggregation. This lists are composed
+    by dictionaries with the following specification:
+
+        * ``Name``:
+            Name of the transformation / aggregation.
+        * ``primitive``:
+            Name of the primitive to apply.
+        * ``init_params``:
+            Dictionary containing the initializing parameters for the primitive.
+
+    Args:
+        transformations (list):
+            List of dictionaries containing the transformation primitives.
+        transformations (list):
+            List of dictionaries containing the aggregation primitives.
+
+    Returns:
+        mlblocks.MLPipeline:
+            An ``MLPipeline`` object that first applies all the transformations
+            and then produces as output the aggregations specified.
+    """
+    primitives = []
+    init_params = {}
+    prefix = []
+    counter = Counter()
+    for transformation in transformations:
+        prefix.append(transformation['name'])
+        primitive = transformation['primitive']
+        counter[primitive] += 1
+        primitive_name = f'{primitive}#{counter[primitive]}'
+        primitives.append(primitive)
+        params = transformation.get('init_params')
+        if params:
+            init_params[primitive_name] = params
+
+    prefix = '.'.join(prefix)
+    outputs = []
+
+    for aggregation in aggregations:
+        aggregation_name = f'{prefix}.{aggregation["name"]}'
+
+        primitive = aggregation['primitive']
+        counter[primitive] += 1
+        primitive_name = f'{primitive}#{counter[primitive]}'
+        primitives.append(primitive)
+
+        primitive = load_primitive(primitive)
+        primitive_outputs = primitive['produce']['output']
+
+        for output in primitive_outputs:
+            output = output['name']
+            outputs.append({
+                'name': f'{aggregation_name}.{output}',
+                'variable': f'{primitive_name}.{output}'
+            })
+
+        params = aggregation.get('init_params')
+        if params:
+            init_params[primitive_name] = params
+
+    return MLPipeline(
+        primitives,
+        init_params=init_params,
+        outputs={'default': outputs}
+    )
+
+
+def _apply_pipeline(row, pipeline, values_column, kwargs):
+    """Apply a ``mlblocks.MLPipeline`` to a row.
+
+    Apply a ``MLPipeline`` to a row of a ``pd.DataFrame``, this function can
+    be combined with the ``pd.DataFrame.apply`` method to be applied to the
+    entire data frame.
+
+    Args:
+        row (pd.Series):
+            Row used to apply the pipeline to.
+        pipeline (mlblocks.MLPipeline):
+            Pipeline to be used for producing the results.
+        values_column (str):
+            The name of the column that contains the signal values.
+        kwargs (dict):
+            Dictionary with additional arguments for the pipeline.
+    """
+    amplitude_values = row[values_column]
+    output = pipeline.predict(
+        amplitude_values=amplitude_values,
+        **kwargs
+    )
+    output_names = pipeline.get_output_names()
+
+    # ensure that we can iterate over output
+    output = output if isinstance(output, tuple) else (output, )
+
+    return pd.Series(dict(zip(output_names, output)))
+
+
+def process_signals(data, transformations, aggregations, keep_values=False,
+                    values_column='values', **kwargs):
     """Process Signals.
 
     The Process Signals is responsible for applying a collection of primitives specified by the
     user in order to create features for the given data.
 
+    Given a list of transformations and aggregations which are composed
+    by dictionaries with the following specification:
+
+        * ``Name``:
+            Name of the transformation / aggregation.
+        * ``primitive``:
+            Name of the primitive to apply.
+        * ``init_params``:
+            Dictionary containing the initializing parameters for the primitive.
+
+    The process signals will build an ``mlblocks.MLPipeline`` and will generate the features
+    by previously applying the transformations and then compute the aggregations.
+
     Args:
         data (pandas.DataFrame):
-            Dataframe with the signal value.
-        primitives (list):
-            A list of python dictionaries with the following keys:
-
-                * ``Name``:
-                    Name of the transformation / aggregation.
-                * ``primitive``:
-                    Name of the primitive to apply.
-                * ``hyperparameters``:
-                    Dictionary containing the hyperparameters of the primitive.
-                * ``primitives``:
-                    List with dictionaries describing the primitives to use for the output of this
-                    primitive. (Allows the same nomenclature as this one).
-
+            Dataframe with a column that contains signal values.
+        transformations (list):
+            List of dictionaries containing the transformation primitives.
+        aggregations (list):
+            List of dictionaries containing the aggregation primitives.
         target_column (str):
-            The column which contains the signal. Defaults to ``values``.
+            The name of the column that contains the signal values. Defaults to ``values``.
+        keep_values (bool):
+            Whether or not to keep the original signal values or remove them.
 
     Returns:
         pandas.DataFrame:
-            A dataframe with new feature columns by applying the previous primitives.
+            A data frame with new feature columns by applying the previous primitives. If
+            ``keep_values`` is ``True`` the original signal values will be conserved in the
+            data frame, otherwise the original signal values will be deleted.
     """
-    pass
+    pipeline = _build_pipeline(transformations, aggregations)
+    features = data.apply(_apply_pipeline, args=(pipeline, values_column, kwargs), axis=1)
+    output = pd.concat([data, features], axis=1)
+    if not keep_values:
+        del output[values_column]
+
+    return output
--- tests/integration/test_process_signals.py
+++ tests/integration/test_process_signals.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+from pandas import Timestamp
+from pandas.testing import assert_frame_equal
+
+from sigpro.demo import get_demo_data
+from sigpro.process_signals import process_signals
+
 
 def test_process_signals():
     """Test the function ``process_signals``.
@@ -9,9 +16,143 @@ def test_process_signals():
 
     Input:
         - data (pd.DataFrame): The demo data.
-        - primitives (list): List of sigpro primitives to apply.
+        - transformations (list): List of sigpro transformations to apply.
+        - aggregations (list): List of sigpro aggregations to apply.
+        - keep_values (bool): Return the values column or delete it.
+        - values_column (str): Column name for the signals values.
+        - kwargs: Additional args to work.
 
     Output:
         - The featurized dataframe.
     """
+    # Setup
+    data = get_demo_data(5)
 
+    transformations = [
+        {
+            'name': 'fft',
+            'primitive': 'sigpro.transformations.frequency.fft.fft_real',
+        },
+        {
+            'name': 'band',
+            'primitive': 'sigpro.transformations.frequency.band.frequency_band',
+            'init_params': {
+                'low': 100,
+                'high': 1000
+            }
+        },
+    ]
+
+    aggregations = [
+        {
+            'name': 'rms',
+            'primitive': 'sigpro.aggregations.amplitude.statistical.rms',
+        },
+        {
+            'name': 'mean',
+            'primitive': 'sigpro.aggregations.amplitude.statistical.mean',
+        },
+    ]
+
+    # run
+    result = process_signals(data, transformations, aggregations, sampling_frequency=10000)
+
+    # assert
+    expected_result = pd.DataFrame({
+        'turbine_id': {
+            0: 'T001',
+            1: 'T001',
+            2: 'T001',
+            3: 'T001',
+            4: 'T001'
+        },
+        'signal_id': {
+            0: 'Sensor1_signal1',
+            1: 'Sensor1_signal1',
+            2: 'Sensor1_signal1',
+            3: 'Sensor1_signal1',
+            4: 'Sensor1_signal1'
+        },
+        'timestamp': {
+            0: Timestamp('2020-01-01 00:00:00'),
+            1: Timestamp('2020-01-01 01:00:00'),
+            2: Timestamp('2020-01-01 02:00:00'),
+            3: Timestamp('2020-01-01 03:00:00'),
+            4: Timestamp('2020-01-01 04:00:00')
+        },
+        'fft.band.rms.rms_value': {
+            0: 13.520619328031275,
+            1: 12.814229400509381,
+            2: 18.34211751830319,
+            3: 11.989327742165269,
+            4: 13.554189188523914
+        },
+        'fft.band.mean.mean_value': {
+            0: 4.784187550373485,
+            1: -2.6254568263243856,
+            2: -3.14619864168385,
+            3: -3.127326866240511,
+            4: -1.0049470688766582
+        },
+    })
+
+    assert_frame_equal(result, expected_result)
+
+
+def test_process_signals_keep_values():
+    """Test the function ``process_signals``.
+
+    Test that ``process_signals`` applies a list of primitives to the demo data and generates
+    new features.
+
+    Input:
+        - data (pd.DataFrame): The demo data.
+        - transformations (list): List of sigpro transformations to apply.
+        - aggregations (list): List of sigpro aggregations to apply.
+        - keep_values (bool): Return the values column or delete it.
+        - values_column (str): Column name for the signals values.
+        - kwargs: Additional args to work.
+
+    Output:
+        - The featurized dataframe.
+    """
+    # Setup
+    transformations = [
+        {
+            'name': 'identity',
+            'primitive': 'sigpro.transformations.amplitude.identity.identity',
+        }
+    ]
+
+    aggregations = [
+        {
+            'name': 'mean',
+            'primitive': 'sigpro.aggregations.amplitude.statistical.mean',
+        },
+    ]
+
+    data = pd.DataFrame({
+        'timestamp': [
+            Timestamp('2020-01-01 00:00:00'),
+        ],
+        'signal_values': [[1, 2, 3]],
+    })
+
+    # run
+    result = process_signals(
+        data,
+        transformations,
+        aggregations,
+        keep_values=True,
+        values_column='signal_values',
+    )
+    # assert
+    expected_result = pd.DataFrame({
+        'timestamp': [
+            Timestamp('2020-01-01 00:00:00'),
+        ],
+        'signal_values': [[1, 2, 3]],
+        'identity.mean.mean_value': [2.0],
+    })
+
+    assert_frame_equal(result, expected_result)
