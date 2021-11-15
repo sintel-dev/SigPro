dad6ec158277f45c3b28997c0decaaeffad36ef0
Frances Hartwell <franceshartwell09@gmail.com>
2021-11-14 20:19:42 -0500

    Bump version: 0.0.4.dev1 → 0.1.0.dev0

--- setup.py
+++ setup.py
@@ -100,6 +100,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sintel-dev/SigPro',
-    version='0.0.4.dev1',
+    version='0.1.0.dev0',
     zip_safe=False,
 )
