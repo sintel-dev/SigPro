fe8cf839a75f3e775211db9e49d9c40d0d109554
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2023-04-06 13:12:44 -0400

    Bump version: 0.1.1.dev0 → 0.1.1

--- setup.py
+++ setup.py
@@ -102,6 +102,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sintel-dev/SigPro',
-    version='0.1.1.dev0',
+    version='0.1.1',
     zip_safe=False,
 )
