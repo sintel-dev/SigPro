8aaca9f0c683483a46aea136ea997ce7f01b53ab
andyx13 <39492936+andyx13@users.noreply.github.com>
2023-11-20 22:35:02 -0500

    modify setup.py to show supported python versions

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
         'Programming Language :: Python :: 3.8',        
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     description='Signal Processing Tools for Machine Mearning',
     entry_points={
